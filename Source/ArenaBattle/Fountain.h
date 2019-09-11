77e6f2ac2de7cd392a58befc294f167fa506cd7b
Godole <godole199@naver.com>
2019-09-11 14:54:54 +0900

    20190911
    asdf

---
+++ Source/ArenaBattle/Fountain.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "GameFramework/Actor.h"
+#include "Fountain.generated.h"
+
+
+UCLASS()
+class ARENABATTLE_API AFountain : public AActor
+{
+	GENERATED_BODY() // 클래스를 언리얼 엔진에서 쓸 수 있도록 환경을 갖춰주는 함수
+	
+public:	
+	// Sets default values for this actor's properties
+	AFountain();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY()
+		UStaticMeshComponent* Body;
+
+	UPROPERTY()
+		UStaticMeshComponent* Water;
+
+	UPROPERTY()
+		UStaticMeshComponent* Light;
+
+	UPROPERTY()
+		UParticleSystemComponent* Splash;
+};
